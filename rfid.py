b621d863f146b856ef95184154f71c0f5364f0eb
lukaszmpiasecki <lukasz.piasecki@int.pl>
2021-03-27 15:37:10 +0100

    Gaszenie swiatla

--- rfid.py
+++ rfid.py
@@ -8,16 +8,13 @@ import time
 BRELOK_1 = 85733152491
 KARTA_1 = 219546064077
 KARTA_2 = 704240555340 
-
-
-
+uprawnieni = {85733152491,219546064077,704240555340}
+obecni = {}
 
 def rfid_odczyt():
     reader = SimpleMFRC522()
-    print("CZEKAM")
     try:
         id, text = reader.read()
-        print("ODCZYT")
     finally:
         GPIO.cleanup()
     return id
@@ -31,12 +28,27 @@ def rfid_zapis(nazwa):
         GPIO.cleanup()
 
 DRZWI_PIN = 17
+LAMPY_PIN = 22
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DRZWI_PIN, GPIO.OUT)
 GPIO.output(DRZWI_PIN, 1)
+GPIO.setup(LAMPY_PIN, GPIO.OUT)
+GPIO.output(LAMPY_PIN, 1)
+
 while True:
-	if rfid_odczyt() == BRELOK_1:
-		print("DOSTEP")
+	if rfid_odczyt() in uprawnieni:
+		if rfid_odczyt() in obecni:
+			del obecni[rfid_odczyt()]
+		else:
+			obecni[rfid_odczyt()] = [rfid_odczyt()]
+		if len(obecni) > 0:
+			GPIO.setmode(GPIO.BCM)
+			GPIO.setup(LAMPY_PIN, GPIO.OUT)
+			GPIO.output(LAMPY_PIN, 0)
+		else:
+			GPIO.setmode(GPIO.BCM)
+			GPIO.setup(LAMPY_PIN, GPIO.OUT)
+			GPIO.output(LAMPY_PIN, 1)
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(DRZWI_PIN, GPIO.OUT)
 		GPIO.output(DRZWI_PIN, 0)
